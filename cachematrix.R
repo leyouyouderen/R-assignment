c9e69da13ab4433ada224bc94ae0f90269edb530
leyouyouderen <leyouyouderen@126.com>
2015-09-01 13:41:30 -0500

    weishiShi
    assignment code

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,36 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+inver=NULL
+set=function(y){
+    x<<-y}
+get=function(){x}
 
+setInver=function(inverse){inver<<-inverse}
+getInver=function(){inver}
+list(set=set,get=get,setInver=setInver,getInver=getInver)
+    
+    
+    
 }
+
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+inver=x$getInver()
+if (!is.null(inver))
+{print("cached number got")
+    return(inver)
+    
+    }
+    else{inver=solve(x$get(),...)
+    x$setInver(inver)
+    return(inver)
+    }
+    
+    
+    }
